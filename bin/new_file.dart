5d55dff76e9b3f7d9639d9062f95ca0b278018ca
Sarwar Murshed Shatil <38027343+shatilsarower@users.noreply.github.com>
2023-04-17 00:45:36 +0600

    First commit

--- bin/new_file.dart
+++ bin/new_file.dart
@@ -2,6 +2,9 @@ void main(){
   Map<String,String> username= {
    "fname" : 'Shatil',
   "lname": 'Alam',
+  "lnam2e": 'Moin',
+  "lname2": 'qwer',
+  "lname45": 'Alam12',
   };
   print(username);
 }
